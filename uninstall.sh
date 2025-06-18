484d898811b6415712a10c2ec004ccf3b20bf29b
xuhui <xuh@koal.com>
2025-06-18 11:15:27 +0800

    允许重复卸载

--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-rm -f /usr/local/bin/lazyvim
+rm -f /usr/local/bin/lazyvim || true
 
-docker rmi registry.cn-hangzhou.aliyuncs.com/xiaocheng2014/lazyvim:base-image
+docker rmi registry.cn-hangzhou.aliyuncs.com/xiaocheng2014/lazyvim:base-image || true
 
 echo "Successfully uninstalled lazyvim command and removed Docker image"
