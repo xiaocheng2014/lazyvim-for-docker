242b1fdd7e8a1bbfd45fcc53ed7fe035a0433d19
xuhui <xuh@koal.com>
2025-06-18 11:35:04 +0800

    镜像地址赋值

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,8 @@ set -e
 
 rm -f /usr/local/bin/lazyvim || true
 
-docker rmi registry.cn-hangzhou.aliyuncs.com/xiaocheng2014/lazyvim:base-image || true
+IMAGE_PATH="registry.cn-hangzhou.aliyuncs.com/xiaocheng2014/lazyvim:base-image"
+
+docker rmi "${IMAGE_PATH}" || true
 
 echo "Successfully uninstalled lazyvim command and removed Docker image"
